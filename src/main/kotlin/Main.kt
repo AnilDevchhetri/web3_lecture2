1b73502f110be517459fdf8d41f6ce2592a0faa3
DELL <chhetrivision@gmail.com>
2023-10-17 11:24:30 +0900

    operator compelete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,30 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program argument this is code")
-    println("this is new updages")
-    println("this is my new updates")
+   operators();
+}
+fun operators(){
+val add = 6 + 2
+println(add) // 8
+
+val minus = 6 - 2
+println(minus) // 4
+
+val mul = 6 * 2
+println(mul) // 12
+
+val divide = 6 / 2
+println(divide) // 3
+
+val divide2 = 7 / 2
+println(divide2) // 3 (!)
+
+val divide3 = 8 / 2
+println(divide3) // 4
+
+val divide4 = 7 / 2.0
+println(divide4) // 3.5 (!)
+
+val remainder = 7 % 2
+println(remainder) // 1
 }
